f9ac08472a280144d7c50c8495f32c9ffb27300c
Farhan Saiyed <saiyedfarhan315@gmail.com>
2025-06-21 10:00:05 +0530

    Day one

---
+++ Conceptss/First-pyton-program/First.py
@@ -0,0 +1,3 @@
+print("Farhan saiyed")
+print(54)
+
---
+++ Conceptss/Modules/MOdules.py
@@ -0,0 +1,24 @@
+# // Think of a module like:
+# A toolbox  that contains tools (functions, variables, classes) you can use in your program.
+
+#  Why use modules?
+# Reusability: You write code once and use it in many programs.
+#
+# Organization: Break large code into smaller, manageable files.
+#
+# Built-in power: You get access to thousands of built-in modules.
+#
+#             Type	                                                   Example
+        # Built-in module	                                       math, random, os
+        # User-defined	                                           Your own .py files
+        # Third-party	                                           numpy, pandas, flask (installed via pip)
+
+
+# Below is the example of using Modules hahah.
+import math
+print("The square root of 25 is ",math.sqrt(25))
+
+import pandas
+print("hi")
+
+
